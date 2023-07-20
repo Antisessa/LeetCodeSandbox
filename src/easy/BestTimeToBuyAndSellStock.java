e1d15be1271258f63dfa26e89f4f039e9699a098
User <nfo@tranco.group>
2023-07-20 14:41:36 +0300

    beat BestTimeToBuyAndSellStock, sadenly, not by own

--- src/easy/BestTimeToBuyAndSellStock.java
+++ src/easy/BestTimeToBuyAndSellStock.java
@@ -2,29 +2,19 @@ package easy;
 
 public class BestTimeToBuyAndSellStock {
     public static void main(String[] args) {
-        int[] prices = {7, 2, 5, 3, 6, 4, 1, 5, 7};
-        int min = prices[0];
-        int max = 0;
-        int nMin=0, nMax=0, result=0;
+        int[] prices = {3,2,6,5,0,3};
+
+        int minprice = Integer.MAX_VALUE; // Максимальное значение для int
+        int maxprofit = 0; // Переменная для результата
 
         for (int i = 0; i < prices.length; i++) {
-            int c = prices[i];
+            if (prices[i] < minprice) // Если следующее число меньше чем минимальное, то присваиваем его минимальным
+                minprice = prices[i];
 
-            if (c < min) {
-                min = c;
-                nMin = i;
-            }
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+            // Если разница между следующим числом и минимальным известным больше чем профит, присваиваем его
         }
-
-        for (int i = nMin; i < prices.length; i++) {
-            int c = prices[i];
-
-            if (c > max) {
-                max = c;
-                nMax = i;
-            }
-        }
-        result = prices[nMax] - prices[nMin];
-        System.out.println(min + " " + max + " " + result);
+        System.out.println(maxprofit);
     }
 }
