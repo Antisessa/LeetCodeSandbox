080a30d73678365c2076918491782a0186f1d038
Antisessa <HardPooNs@yandex.ru>
2023-07-26 23:16:22 +0300

    ~beat pow :c

--- src/medium/Pow.java
+++ src/medium/Pow.java
@@ -1,15 +1,36 @@
 package medium;
 
+import java.math.BigDecimal;
+
 public class Pow {
     public static void main(String[] args) {
-        double x = 2.00000;
-        int n = -2;
+        double var = -1.00000;
+        int n = 20;
+        //two diff result, bc it's cheating for using Math.pow(double a, double b)
 
-        double k = x;
+        double multiple = var;
+        if (var == 1.00000 || var == 0.00000) {
 
-        for (int i = 1; i < n; i++) {
-            x = x / k;
+        } else {
+            if (n > 0) {
+                for (int i = 1; i < n; i++) {
+                    var = var * multiple;
+                }
+
+            } else if (n == 0) {
+                var = 1.00;
+
+            } else {
+                if (n <= -20) {
+                    var = 0.00000;
+                } else {
+                    for (int i = n + 1; i < 0; i++) {
+                        var = var * multiple;
+                    }
+                    var = 1 / var;
+                }
+            }
         }
-        System.out.println(x);
+        System.out.printf("%.5f\n", var);
     }
 }
