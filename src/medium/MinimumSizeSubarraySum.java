8a1b35d6a19a4a2d66d164844c5cb91e14b71470
User <nfo@tranco.group>
2023-07-30 19:38:44 +0300

    try to beat MinimumSizeSubarraySum

--- src/medium/MinimumSizeSubarraySum.java
+++ src/medium/MinimumSizeSubarraySum.java
@@ -1,29 +1,48 @@
 package medium;
 
+import java.util.ArrayList;
+
 public class MinimumSizeSubarraySum {
     public static void main(String[] args) {
         //input
-        int target = 7;
-        int[] nums = {2,3,1,2,4,3};
+        int target = 1_000_000_000;
+//        int[] nums = {1,2,3,4,5};
 
-        int k = 0;
-        int resultLength = Integer.MAX_VALUE;
+        int[] nums = new int[10000];
 
         for (int i = 0; i < nums.length; i++) {
-            k = i + 1;
-            int currentSum = nums[i] + nums[k];
+            nums[i] = 10000;
+        }
 
-            while (currentSum < target && k < nums.length) {
-                currentSum = currentSum + nums[k];
-                k++;
 
+        int k = 0;
+        int currentLength = 0;
+        int resultLength = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length && currentLength != 1; i++) {
+            int currentSum = nums[i];
+
+            if(currentSum < target && i!= nums.length-1) {
+                k = i + 1;
+                currentSum = nums[i] + nums[k];
+
+                while (currentSum < target && k < nums.length - 1) {
+                    k++;
+                    currentSum = currentSum + nums[k];
+                }
+
+                if (currentSum >= target)
+                    currentLength = k - i + 1;
+//                else if(currentSum >= target && i == 0) {
+//                    currentLength = k - 1;
+
+            } else {
+                if (currentSum >= target)
+                currentLength = 1;
             }
 
-            int currentLength = k - i + 1;
             if (resultLength > currentLength)
                 resultLength = currentLength;
-
-            i = k;
         }
         System.out.println(resultLength);
     }
--- src/test.java
+++ src/test.java
@@ -4,84 +4,26 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-//        ListNode l1_21 = new ListNode(1);
-//        ListNode l1_20 = new ListNode(0, l1_21);
-//        ListNode l1_19= new ListNode(0, l1_20);
-//        ListNode l1_18 = new ListNode(0, l1_19);
-//        ListNode l1_17 = new ListNode(0, l1_18);
-//        ListNode l1_16= new ListNode(0, l1_17);
-//        ListNode l1_15 = new ListNode(0, l1_16);
-//        ListNode l1_14 = new ListNode(0, l1_15);
-//        ListNode l1_13 = new ListNode(0, l1_14);
-//        ListNode l1_12 = new ListNode(0, l1_13);
-//        ListNode l1_11 = new ListNode(0, l1_12);
-//        ListNode l1_10 = new ListNode(0, l1_11);
-//        ListNode l1_9 = new ListNode(0, l1_10);
-//        ListNode l1_8 = new ListNode(0, l1_9);
-//        ListNode l1_7 = new ListNode(0, l1_8);
-//        ListNode l1_6 = new ListNode(0, l1_7);
-//        ListNode l1_5 = new ListNode(0, l1_6);
-//        ListNode l1_4 = new ListNode(0, l1_5);
-//        ListNode l1_3 = new ListNode(0, l1_4);
-//        ListNode l1_2 = new ListNode(0, l1_3);
-//        ListNode l1_1 = new ListNode(0, l1_2);
-//        ListNode l1 = new ListNode(1, l1_1);
+        int[] nums = {1,2,3,4,5};
+        int target = 11;
 
-        ListNode l1_2 = new ListNode(3);
-        ListNode l1_1 = new ListNode(4, l1_2);
-        ListNode l1 = new ListNode(2, l1_1);
-
-        ListNode l2_2 = new ListNode(4);
-        ListNode l2_1 = new ListNode(6, l2_2);
-        ListNode l2 = new ListNode(5, l2_1);
-
-        ListNode dummyHead = new ListNode(0);
-        ListNode tail = dummyHead;
-        int carry = 0;
-
-        while (l1 != null || l2 != null || carry != 0) {
-            int digit1;
-            if (l1 != null) digit1 = l1.val;
-            else digit1 = 0;
-
-            int digit2;
-            if (l2 != null) digit2 = l2.val;
-            else digit2 = 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode newNode = new ListNode(digit);
-            tail.next = newNode;
-            tail = tail.next;
-
-            if (l1 != null){
-                l1 = l1.next;
-            }
-            else {
-                l1 = null;
-            }
-
-            if (l2 != null){
-                l2 = l2.next;
-            }
-            else {
-                l2 = null;
+        int ans = Integer.MAX_VALUE, j = 0, sum = 0;
+        for(int i=0; i < nums.length; i++)
+        {
+            sum += nums[i];
+            if(sum>=target)
+            {
+                while(sum>=target)
+                {
+                    j++;
+                    sum-=nums[j];
+                }
+                ans = Math.min (ans, i-j+2);
             }
         }
+        if (ans == nums.length + 1)
+            ans = 0;
 
-        ListNode result = dummyHead.next;
-        dummyHead.next = null;
-
-        ArrayList numsResult = new ArrayList();
-        ListNode list3 = result;
-        while(list3.next != null) {
-            numsResult.add(list3.val);
-            list3 = list3.next;
-        }
-        numsResult.add(list3.val);
-
-        System.out.println(numsResult);
+        System.out.println(ans);
     }
 }
