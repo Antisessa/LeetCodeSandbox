57662764a2ec90a5630f26839cebd229228c76da
Antisessa <118179929+Antisessa@users.noreply.github.com>
2023-08-01 12:06:23 +0300

    beat MinimumSizeSubarraySum

--- src/medium/MinimumSizeSubarraySum.java
+++ src/medium/MinimumSizeSubarraySum.java
@@ -1,49 +1,29 @@
 package medium;
 
-import java.util.ArrayList;
-
 public class MinimumSizeSubarraySum {
     public static void main(String[] args) {
         //input
-        int target = 1_000_000_000;
-//        int[] nums = {1,2,3,4,5};
+        int[] nums = {2,3,1,2,4,3};
+        int target = 7;
 
-        int[] nums = new int[10000];
+        int ans = nums.length+1, j = 0, sum = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = 10000;
-        }
+        for(int i=0; i < nums.length; i++) {
+            sum += nums[i];
 
-
-        int k = 0;
-        int currentLength = 0;
-        int resultLength = Integer.MAX_VALUE;
-
-        for (int i = 0; i < nums.length && currentLength != 1; i++) {
-            int currentSum = nums[i];
-
-            if(currentSum < target && i!= nums.length-1) {
-                k = i + 1;
-                currentSum = nums[i] + nums[k];
-
-                while (currentSum < target && k < nums.length - 1) {
-                    k++;
-                    currentSum = currentSum + nums[k];
+            if(sum>=target){
+                while(sum>=target) {
+                    sum-=nums[j];
+                    j++;
                 }
-
-                if (currentSum >= target)
-                    currentLength = k - i + 1;
-//                else if(currentSum >= target && i == 0) {
-//                    currentLength = k - 1;
-
-            } else {
-                if (currentSum >= target)
-                currentLength = 1;
+                ans = Math.min (ans, i-j+2);
             }
 
-            if (resultLength > currentLength)
-                resultLength = currentLength;
         }
-        System.out.println(resultLength);
+
+        if (ans == nums.length + 1)
+            ans = 0;
+
+        System.out.println(ans);
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,26 +1,23 @@
-import medium.ListNode;
-
-import java.util.*;
-
 public class test {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
-        int target = 11;
+        int[] nums = {2,3,1,2,4,3};
+        int target = 7;
 
-        int ans = Integer.MAX_VALUE, j = 0, sum = 0;
-        for(int i=0; i < nums.length; i++)
-        {
+        int ans = nums.length+1, j = 0, sum = 0;
+
+        for(int i=0; i < nums.length; i++) {
             sum += nums[i];
-            if(sum>=target)
-            {
-                while(sum>=target)
-                {
-                    j++;
+
+            if(sum>=target){
+                while(sum>=target) {
                     sum-=nums[j];
+                    j++;
                 }
                 ans = Math.min (ans, i-j+2);
             }
+
         }
+
         if (ans == nums.length + 1)
             ans = 0;
 
